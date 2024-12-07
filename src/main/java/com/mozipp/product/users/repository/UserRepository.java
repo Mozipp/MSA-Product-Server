881720dc170bc1ca49b5b763b3c363d32ef3cfd1
so1eeee <soljeong821@gmail.com>
2024-12-08 02:51:15 +0900

    chore : add repository annotation

--- src/main/java/com/mozipp/product/users/repository/UserRepository.java
+++ src/main/java/com/mozipp/product/users/repository/UserRepository.java
@@ -2,11 +2,11 @@ package com.mozipp.product.users.repository;
 
 import com.mozipp.product.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByUsername(String username);
-
     Optional<User> findByUsername(String username);
 }
